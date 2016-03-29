6bb612998c840fe57450a04244a8c72d15c28f59
JoaoG <jvictor96jv@gmail.com>
2016-03-29 09:57:53 -0300

    Trade Plugin

---
+++ src/main/java/ghignatti/joao/plugin/event/inventory/InventoryClick.java
@@ -0,0 +1,12 @@
+package ghignatti.joao.plugin.event.inventory;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class InventoryClick implements Listener {
+
+    public void onInventoryCLick(InventoryClickEvent event) {
+
+
+    }
+}
